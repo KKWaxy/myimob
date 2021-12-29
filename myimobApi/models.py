79bea10c869597b7fdbab7e58e5fb3573fb94103
Wilfried KOUASSI <kkwaxy@gmail.com>
2021-12-29 01:39:42 +0000

    Update of House fields

--- myimobApi/models.py
+++ myimobApi/models.py
@@ -8,7 +8,11 @@ class Illustration(models.Model):
 class Category(models.Model):
     pass
 class House(models.Model):
-    
+    address = models.CharField(_("Adresse"), max_length=500, null=True, blank=True)
+    LotSize = models.CharField(_("Taille du lot"), max_length=600, null=True, blank=True)
+    latitude = models.CharField(_("Latitude"), max_length=10, null=True, blank=True)
+    longitude = models.CharField(_("Longitude"), max_length=10, null=True, blank=True)
+    numeroLot = models.CharField(_("Numéro du Lot"), max_length=20, null=True, blank=True)
     label = models.CharField(_("Libelle"), max_length=200)
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE)
     create_date =  models.DateTimeField(_("Created Date"), auto_now_add=True)
